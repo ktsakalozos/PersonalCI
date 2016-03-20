0a11a3f55fe7a1d12f6092e8ab1a7621193701f3
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2016-03-20 23:35:06 +0200

    Fixes in setup env

--- setup_env.sh
+++ setup_env.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 #
 
-sudo apt-get -y install juju juju-local charm-tools git
-juju init
-juju bootstrap -e local
-mkdir -p ~/workspace/charms/layers
-echo "export JUJU_REPOSITORY=$HOME/workspace/charms" >> ~/.bashrc
-echo "export LAYER_PATH=$JUJU_REPOSITORY/layers" >> ~/.bashrc
-echo "export INTERFACE_PATH=$JUJU_REPOSITORY/interfaces" >> ~/.bashrc
-source ~/.bashrc
-
+sudo apt-get -y install juju juju-local charm-tools git python3
 sudo pip3 install gitpython
+juju init -f
+juju bootstrap -e local 
+juju switch local
+juju status
+#mkdir -p ~/workspace/charms/layers
+#echo "export JUJU_REPOSITORY=$HOME/workspace/charms" >> ~/.bashrc
+#echo "export LAYER_PATH=$JUJU_REPOSITORY/layers" >> ~/.bashrc
+#echo "export INTERFACE_PATH=$JUJU_REPOSITORY/interfaces" >> ~/.bashrc
+#source ~/.bashrc
+
